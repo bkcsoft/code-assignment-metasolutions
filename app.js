1b19c33f8af21e733826c92d5d61d2cb5371a3ad
Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com>
2015-11-17 03:58:59 +0100

    And now it's actually working again (properly)

--- app.js
+++ app.js
@@ -6,38 +6,35 @@ angular.module('app', [])
 		"use strict";
 		$scope.comments = [];
 		$scope.added = true;
-		$scope.temp = {};
-		$scope.tempComment = "";
+		$scope.temp = {comment: '', selection: null};
 		$scope.setTempComment = function() {
 			var sel = rangy.getSelection().saveRanges();
 			console.log(sel);
-			$scope.temp = sel;
+			$scope.temp.selection = sel;
 		};
 		$scope.showAddBox = function() {
-			if($scope.temp !== {}) {
+			if($scope.temp.selection !== null) {
 				$scope.added = false;
+			} else {
+				console.log("Nothing selected stopid...");
 			}
 		}
 		$scope.closeAddBox = function() {
-			$scope.added = false;
-			$scope.temp = {};
-			$scope.tempComment = "";
+			$scope.added = true;
+			$scope.temp = {comment: '', selection: null};
 		}
 		$scope.addComment = function() {
-			if($scope.temp !== {} && $scope.tempComment !== '') {
-				$scope.temp.comment = $scope.tempComment;
+			if($scope.temp.selection !== null && $scope.temp.comment !== '') {
 				$scope.comments.push($scope.temp);
-				$scope.temp = {};
-				$scope.tempComment = "";
-				$scope.added = true;
+				$scope.closeAddBox();
 			}
 		};
 		$scope.showComment = function(idx) {
 			var comment = $scope.comments[idx];
 			console.log("Showing " + comment.comment);
 			var sel = rangy.getSelection();
-			sel.restoreRanges($scope.comments[idx]);
-			$scope.temp = sel.saveRanges();
+			sel.restoreRanges($scope.comments[idx].selection);
+			$scope.temp.selection = sel.saveRanges();
 		};
 		$scope.remComment = function(idx) {
 			$scope.comments.splice(idx, 1);
